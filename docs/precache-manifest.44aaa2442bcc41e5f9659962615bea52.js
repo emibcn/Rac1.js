e3206176b662525077a56784c6c330c7e17c9525
emibcn <emi@algorismia.com>
2018-12-08 18:17:20 +0100

    Use GA from within a React comoponent

---
+++ docs/precache-manifest.44aaa2442bcc41e5f9659962615bea52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3417ba4f08baefed94d8",
+    "url": "/Rac1.js/static/js/runtime~main.3417ba4f.js"
+  },
+  {
+    "revision": "f1185b4b5594a0f2945b",
+    "url": "/Rac1.js/static/js/main.f1185b4b.chunk.js"
+  },
+  {
+    "revision": "bbf3a0ad2e22b43d3e48",
+    "url": "/Rac1.js/static/js/1.bbf3a0ad.chunk.js"
+  },
+  {
+    "revision": "f1185b4b5594a0f2945b",
+    "url": "/Rac1.js/static/css/main.457fc9bb.chunk.css"
+  },
+  {
+    "revision": "bbf3a0ad2e22b43d3e48",
+    "url": "/Rac1.js/static/css/1.fd6e19d4.chunk.css"
+  },
+  {
+    "revision": "786433d91e76290fcb449909f3d2d21a",
+    "url": "/Rac1.js/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Rac1.js/precache-manifest.32f8731707adc4a556530c20dbd84d59.js"
+  "/Rac1.js/precache-manifest.44aaa2442bcc41e5f9659962615bea52.js"
 );
 
 workbox.clientsClaim();
