e1d1dfa0717abd656dc54b49766edd730f666dc9
emibcn <emi@algorismia.com>
2019-01-01 20:46:24 +0100

    PodcastList: Make it responsive: multicolumn only for big devices

--- app/src/PodcastsList.jsx
+++ app/src/PodcastsList.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import MediaQuery from 'react-responsive';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 class PodcastsList extends React.PureComponent {
 
-  render() {
+  renderChildren() {
     const { children, current } = this.props;
+    return children === undefined || children.length === 0 ?
+      null : children.map( (child, index) =>
+          <li
+            key={ child.key }
+            style={{
+              position: 'relative',
+              marginLeft: '2.5em',
+              textIndent: '-1.5em',
+              padding: '1pt 0',
+            }}
+          >
+            { index === current ? (
+              <FontAwesomeIcon
+                icon={ faPlay }
+                style={{
+                  position: 'absolute',
+                  left: '-2.75em',
+                  padding: '1pt 0',
+                }}/>
+            ) : null
+            }
+            { child }
+          </li>
+        )
+  }
+
+  render() {
+    const { children } = this.props;
+    const styles = {
+      listStyleType: 'none',
+      marginLeft: 0,
+      paddingLeft: 0,
+      textAlign: 'left',
+    };
 
     return children === undefined || children.length === 0 ?
-      null :
-      (
-        <ul style={{
-          listStyleType: 'none',
-          marginLeft: 0,
-          paddingLeft: 0,
-          textAlign: 'left',
-          columnCount: Math.floor(children.length / 10) || 1,
-        }}>
-          { children.map( (child, index) =>
-            <li
-              key={ child.key }
-              style={{
-                position: 'relative',
-                marginLeft: '2.5em',
-                textIndent: '-1.5em',
-                padding: '1pt 0',
-              }}
-            >
-              { index === current ? (
-                <FontAwesomeIcon
-                  icon={ faPlay }
-                  style={{
-                    position: 'absolute',
-                    left: '-2.75em',
-                    padding: '1pt 0',
-                  }}/>
-              ) : null
-              }
-              { child }
-            </li>
-          )}
-        </ul>
+      null : (
+        <MediaQuery minWidth={700}>
+          { matches => {
+            return (
+              <ul style={{
+                ...styles,
+                columnCount: matches ? Math.floor(children.length / 10) || 1 : 1,
+              }}>
+                { this.renderChildren() }
+              </ul>
+            )
+          }}
+        </MediaQuery>
       )
   }
 }
