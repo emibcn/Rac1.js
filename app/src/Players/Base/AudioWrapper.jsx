8af94460f5f498c003f86b609c9b5ba073fbdfee
emibcn <emi@algorismia.com>
2020-09-05 15:48:38 +0200

    Create AudioWrapper component to wrap audio player and controls into a single container, allowing to greatly simplify both players. Optimize Players event handlers using class attribute arrow functions to statically bind them to `this`

---
+++ app/src/Players/Base/AudioWrapper.jsx
@@ -0,0 +1,222 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import { Helmet } from 'react-helmet';
+
+import ReactAudioPlayer from 'react-audio-player';
+import Controls from './Controls';
+import MediaSession from './MediaSession';
+
+class AudioWrapper extends PureComponent {
+  
+  _player = React.createRef();
+
+  constructor(props) {
+    super();
+    // Initial state
+    this.state = {
+      volume: 1,
+      muted: false,
+      isPlaying: false,
+      showAdvancedControls: false,
+      ...this.getHideButtons(),
+    };
+  }
+
+  componentWillUnmount() {
+    // Unregister player event listeners
+    const player = this.getPlayer().current;
+    if ( player && player.removeEventListener ) {
+      player.removeEventListener('play', this.handlePlayStatusChangeTrue);
+      player.removeEventListener('pause', this.handlePlayStatusChangeFalse);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if ( prevProps.path !== this.props.path ) {
+      // Force status to pause. If it will autoplay,
+      // it will change again to playing = true
+      this.handlePlayStatusChangeFalse();
+      this.setState( this.getHideButtons() );
+    }
+  }
+
+  /////////////////////////////
+  // Actions for our Controls
+  // Bound to this when needed
+  /////////////////////////////
+  getPlayer = () => this._player.audioEl
+
+  // Actions activated through MediaSession
+  // Call Controls.runAction to ensure all work coherently:
+  // - Logs
+  // - Tracking
+  // - No duplicated code
+  handleSeekBackward = () => this.controlsRunAction('-10s',  'MediaSession')
+  handleSeekForward  = () => this.controlsRunAction('+10s',  'MediaSession')
+  handlePlay         = () => this.controlsRunAction('Play',  'MediaSession')
+  handlePause        = () => this.controlsRunAction('Pause', 'MediaSession')
+  handlePlayPrev     = () => this.controlsRunAction('Prev',  'MediaSession')
+  handlePlayNext     = () => this.controlsRunAction('Next',  'MediaSession')
+
+  // Play status may change from our Constrols button or from the <audio> tag
+  // itself (or whatever associated, as from notifications screen controls)
+  handlePlayStatusChange(isPlaying) {
+    this.setState({ isPlaying });
+  }
+
+  onSetVolume = volume => this.setState({ volume })
+  onSetMuted  = muted => this.setState({ muted })
+
+  onSetVolumeAndMuted = e => {
+    this.onSetVolume(e.currentTarget.volume);
+    this.onSetMuted(e.currentTarget.muted);
+  }
+  isSeekable = () => {
+    const player = this.getPlayer();
+    return (
+      player &&
+      player.current &&
+      player.current.duration !== Infinity
+    )
+  }
+  getHideButtons = () => {
+    return {
+      hideButtons: [
+        ...( this.isSeekable()
+              ? []
+              : ['-10m', '-60s', '-10s', '+10m', '+60s', '+10s']
+        ),
+        ...(!this.props || !this.props.onPlayNext ? ['Next'] : []),
+        ...(!this.props || !this.props.onPlayPrev ? ['Prev'] : []),
+      ]
+    }
+  }
+  onLoadedMetadata = () => this.setState( this.getHideButtons() );
+  onShowAdvancedChange = show => {
+    this.setState({ showAdvancedControls: show });
+    return `${show ? 'Show' : 'Hide'} advanced buttons`
+  }
+
+  // Create bound functions for setting specific playing status 
+  handlePlayStatusChangeTrue = this.handlePlayStatusChange.bind(this, true);
+  handlePlayStatusChangeFalse = this.handlePlayStatusChange.bind(this, false);
+
+  // Bound functions for getting refs from Controls and audio player
+  refPlayer = el => { this._player = el }
+  refControls = el => {
+    if ( el ) {
+      this.controlsKeyHandlerFocus = el.keyHandlerFocus;
+      this.controlsRunAction = el.runAction;
+    }
+  }
+
+  render() {
+    const {
+      volume,
+      muted,
+      isPlaying,
+      showAdvancedControls,
+      hideButtons
+    } = this.state;
+    const {
+      autoPlay,
+      title,
+      titleHead,
+      path,
+      allowFocus,
+      onEnded,
+      onPlayNext,
+      onPlayPrev,
+      extraControls,
+      volumeAsAdvanced,
+      artist,
+      album,
+      image,
+    } = this.props;
+    const isSeekable = this.isSeekable();
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '.5em',
+      }}>
+        <Helmet>
+          <title>{ titleHead }</title>
+        </Helmet>
+        <MediaSession
+          title={ titleHead }
+          artist={ artist }
+          album={ album }
+          image={ image }
+          /*
+             Set actions for MediaSession (for controls on
+             notification screen and similars).
+             Undefined actions are not shown.
+          */
+          onPlay={ this.handlePlay }
+          onPause={ this.handlePause }
+          onPlayPrev={ onPlayPrev ? this.handlePlayPrev : undefined }
+          onPlayNext={ onPlayNext ? this.handlePlayNext : undefined }
+          onSeekBackward={ isSeekable ? this.handleSeekBackward : undefined }
+          onSeekForward={ isSeekable ? this.handleSeekForward : undefined }
+        />
+        <h3>{ title }</h3>
+        <Controls
+            getPlayer={ this.getPlayer }
+            volume={ volume }
+            muted={ muted }
+            allowFocus={ allowFocus }
+            onPlayNext={ onPlayNext }
+            onPlayPrev={ onPlayPrev }
+            onSetVolume={ this.onSetVolume }
+            onSetMuted={ this.onSetMuted }
+            showAdvanced={ showAdvancedControls }
+            volumeAsAdvanced={ volumeAsAdvanced }
+            onShowAdvancedChange={ this.onShowAdvancedChange }
+            isPlaying={ isPlaying }
+            ref={ this.refControls }
+            extraControls={ extraControls }
+            hideButtons={ hideButtons }
+          />
+        <ReactAudioPlayer
+            ref={ this.refPlayer }
+            style={{ width: '100%' }}
+            src={ path }
+            autoPlay={ autoPlay }
+            title={ title }
+            controls
+            controlsList='nodownload'
+            preload={ (autoPlay ? 'auto' : 'metadata') }
+            onEnded={ onEnded }
+            volume={ volume }
+            muted={ muted }
+            onPlay={ this.handlePlayStatusChangeTrue }
+            onPause={ this.handlePlayStatusChangeFalse }
+            onVolumeChanged={ this.onSetVolumeAndMuted }
+            onLoadedMetadata={ this.onLoadedMetadata }
+          />
+      </div>
+    );
+  }
+}
+
+AudioWrapper.propTypes = {
+  onPlayPrev: PropTypes.func,
+  onPlayNext: PropTypes.func,
+  allowFocus: PropTypes.func,
+  extraControls: Controls.propTypes.extraControls,
+  volumeAsAdvanced: PropTypes.bool,
+  autoPlay: PropTypes.bool,
+  path: PropTypes.string,
+  title: PropTypes.string,
+  titleHead: PropTypes.string,
+  artist: PropTypes.string,
+  album: PropTypes.string,
+};
+
+export default AudioWrapper;
--- app/src/Players/Rac1/Rac1Directe.jsx
+++ app/src/Players/Rac1/Rac1Directe.jsx
@@ -1,47 +1,44 @@
 import React, { Component } from 'react';
 
 import { translate } from 'react-translate';
-import { Helmet } from 'react-helmet';
-import ReactAudioPlayer from 'react-audio-player';
 
 import { Rac1Live } from '../../Backends';
-import Controls from '../Base/Controls';
+
+import AudioWrapper from '../Base/AudioWrapper';
 import PodcastCover from '../Base/PodcastCover';
-import MediaSession from '../Base/MediaSession';
 
 class Rac1Directe extends Component {
 
   constructor(props) {
     super();
 
-    this.rac1 = new Rac1Live({
+    this.backend = new Rac1Live({
 
       // Get data from backend
-      onUpdate: this.handleUpdate.bind(this),
+      onUpdate: this.handleUpdate,
 
       // Get errors from backend
-      onError: this.handleError.bind(this),
+      onError: this.handleError,
 
     });
 
+    this.extraControls = [];
+
     // Initial state
     this.state = {
-      volume: 1,
-      muted: false,
-      isPlaying: false,
       podcast: false,
       hasError: false,
       error: {},
     };
   }
 
-  handleUpdate(podcast) {
+  handleUpdate = (podcast) => {
     this.setState({ podcast });
   }
 
   // Saves errors from backend into state so they
   // can be reraised into ReactDOM tree and catched correctly
-  handleError(error) {
+  handleError = (error) => {
     error.message = `Rac1Directe: ${error.message}`;
     this.setState({
       hasError: true,
@@ -50,84 +47,59 @@ class Rac1Directe extends Component {
   }
 
   componentWillUnmount() {
-    // Unregister player event listeners
-    const player = this.getPlayer().current;
-    if ( player && player.removeEventListener ) {
-      console.log("Remove audio event listeners");
-      player.removeEventListener('play', this.handlePlayStatusChangeTrue);
-      player.removeEventListener('pause', this.handlePlayStatusChangeFalse);
-    }
-
     // Abort backend fetches
-    this.rac1.abort();
+    this.backend.abort();
   }
 
   render() {
-    const { volume, muted, isPlaying, podcast, hasError, error } = this.state;
-    const { t } = this.props;
-    const currentPath = podcast !== undefined ? podcast.path : '';
-    const autoplay = true;
-    const title = t('Rac1 live');
+    const { podcast, hasError, error } = this.state;
 
     // If we have a backend error, reraise into ReactDOM tree
     if ( hasError ) {
       throw Error(error);
     }
 
+    const { t } = this.props;
+    const currentPath = podcast !== undefined ? podcast.path : '';
+    const titleHead =
+      (
+        !podcast ||
+        !podcast.title
+          ? ''
+          : `${podcast.title} | `
+      ) +
+      t(this.backend.name);
+    const title =
+      t(this.backend.name) + (
+        !podcast ||
+        !podcast.title
+          ? ''
+          : `: ${podcast.title}`
+      );
+
     return (
-      <>
-        <Helmet>
-          <title>{ t("Live") }</title>
-        </Helmet>
-        <MediaSession
-           title={ `${podcast.title} | ${title}` }
-           artist={ podcast.author }
-           album={ podcast.schedule }
-           image={ podcast.image }
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-evenly',
+      }}>
+        <AudioWrapper
+          autoPlay={ true }
+          title={ title }
+          path={ currentPath }
+          allowFocus={ this.allowFocus }
+          extraControls={ this.extraControls }
+          volumeAsAdvanced={ false }
+          titleHead={ titleHead }
+          artist={ podcast.author }
+          album={ podcast.schedule }
+          image={ podcast.image }
         />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-evenly',
-        }}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              padding: '.5em',
-          }}>
-            <h3>{ title }</h3>
-            <Controls
-              getPlayer={ this.getPlayer }
-              volume={ volume }
-              muted={ muted }
-              allowFocus={ this.allowFocus }
-              isPlaying={ isPlaying }
-              onSetVolume={ this.onSetVolume }
-              onSetMuted={ this.onSetMuted }
-              hideButtons={['Prev', 'Next', '-10m', '-60s', '-10s', '+10m', '+60s', '+10s']}
-            />
-            <ReactAudioPlayer
-              ref={ this.refPlayer }
-              style={{ width: '100%' }}
-              src={ currentPath }
-              autoPlay={ autoplay }
-              title={ title }
-              controls
-              controlsList='nodownload'
-              preload={ (autoplay ? 'auto' : 'metadata') }
-              volume={ volume }
-              muted={ muted }
-              onPlay={ this.handlePlayStatusChangeTrue }
-              onPause={ this.handlePlayStatusChangeFalse }
-              onVolumeChanged={ this.onSetVolumeAndMuted }
-            />
-          </div>
-          { podcast !== false ? (
+        { podcast !== false ? (
+            <>
+              <div style={{ width: '.5em', height: '.5em' }} />
               <PodcastCover
                 minWidth={ 708 }
                 image={ podcast.image }
@@ -136,36 +108,14 @@ class Rac1Directe extends Component {
                 author={ podcast.author }
                 schedule={ podcast.schedule }
               />
-            ) : null
-          }
-        </div>
-      </>
+            </>
+          ) : null
+        }
+      </div>
     );
   }
 
-  handlePlayStatusChange(isPlaying) {
-    this.setState({ isPlaying });
-  }
-
-  handlePlayStatusChangeTrue = this.handlePlayStatusChange.bind(this, true);
-  handlePlayStatusChangeFalse = this.handlePlayStatusChange.bind(this, false);
-
-  _refPlayer = el => { this._player = el }
-  _getPlayer = () => this._player.audioEl
-
-  refPlayer = this._refPlayer.bind(this)
-  getPlayer = this._getPlayer.bind(this)
-
   allowFocus = el => el.className.match( /rc-slider-handle|ReactModal/ )
-  _onSetVolume = volume => this.setState({ volume })
-  _onSetMuted = muted => this.setState({ muted })
-  _onSetVolumeAndMuted = e => {
-    this.onSetVolume(e.currentTarget.volume);
-    this.onSetMuted(e.currentTarget.muted);
-  }
-  onSetVolume          = this._onSetVolume.bind(this)
-  onSetMuted           = this._onSetMuted.bind(this)
-  onSetVolumeAndMuted  = this._onSetVolumeAndMuted.bind(this)
 
 }
 
