5800a7fc570b342f02a5e05c899e9ce828f8afb3
emibcn <emi@algorismia.com>
2021-10-16 17:54:49 +0200

    GAListener: withGAEvent: Fix Prefer React components to have a `shouldComponentUpdate` method - JS-0466

--- app/src/GAListener/withGAEvent.js
+++ app/src/GAListener/withGAEvent.js
@@ -5,20 +5,18 @@ import GAEventContext from './GAEventContext'
 // HOC to add `sendEvent` function respecting refs
 export function withGAEvent (Component) {
   // Wrapper component
-  class GAEvent extends React.Component {
-    render () {
-      const { forwardedRef, ...props } = this.props
+  function GAEvent (props) {
+    const { forwardedRef, ...restProps } = props
 
-      // Get `sendEvent` from context and pass it to the wrapped
-      // component, along with ref and the rest of props
-      return (
-        <GAEventContext.Consumer>
-          {(sendEvent) => (
-            <Component ref={forwardedRef} {...props} {...{ sendEvent }} />
-          )}
-        </GAEventContext.Consumer>
-      )
-    }
+    // Get `sendEvent` from context and pass it to the wrapped
+    // component, along with ref and the rest of props
+    return (
+      <GAEventContext.Consumer>
+        {(sendEvent) => (
+          <Component ref={forwardedRef} {...restProps} {...{ sendEvent }} />
+        )}
+      </GAEventContext.Consumer>
+    )
   }
 
   // Return wrapper respecting ref
