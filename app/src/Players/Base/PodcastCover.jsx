3d637e7f384ebf1d4a0b3ddbb8fd95244ff40c95
emibcn <emi@algorismia.com>
2021-10-16 17:00:58 +0200

    Fix #125 - DeepSource: Fix unused vars

--- app/src/Players/Base/PodcastCover.jsx
+++ app/src/Players/Base/PodcastCover.jsx
@@ -4,67 +4,65 @@ import PropTypes from 'prop-types';
 import { translate } from 'react-translate';
 import MediaQuery from 'react-responsive';
 
-class PodcastCover extends PureComponent {
-  render() {
-    const {
-      minWidth,
-      image,
-      programUrl,
-      title,
-      author,
-      schedule,
-      t
-    } = this.props;
+function PodcastCover (props) {
+  const {
+    minWidth,
+    image,
+    programUrl,
+    title,
+    author,
+    schedule,
+    t
+  } = this.props;
 
-    return (
-      <MediaQuery minWidth={ minWidth }>
-        { matches => (
-           <article
+  return (
+    <MediaQuery minWidth={ minWidth }>
+      { matches => (
+         <article
+           style={{
+             display: 'flex',
+             flexDirection: matches ? 'row' : 'column',
+             flexWrap: matches ? 'nowrap' : 'wrap',
+             justifyContent: 'center',
+             backgroundColor: 'white',
+             color: '#333',
+             padding: '1em',
+             borderRadius: '10px',
+           }}>
+           <header
              style={{
                display: 'flex',
-               flexDirection: matches ? 'row' : 'column',
-               flexWrap: matches ? 'nowrap' : 'wrap',
-               justifyContent: 'center',
-               backgroundColor: 'white',
-               color: '#333',
-               padding: '1em',
-               borderRadius: '10px',
-             }}>
-             <header
-               style={{
-                 display: 'flex',
-                 flexDirection: 'column',
-                 alignItems: 'center',
-             }}>
-               <h3>
-                 <a href={ programUrl } target='_blank' rel="noopener noreferrer">{ title }</a>
-               </h3>
-               <div style={{ marginBottom: '.5em' }}>
-                 { schedule }
-               </div>
-               <h4>{ author }</h4>
-             </header>
-             <div
-               style={{
-                 alignSelf: 'center',
-             }}>
-               <figure style={{ position: 'relative' }} aria-label={ `${t("Author")}: ${author}` }>
-                 <img
-                   src={ image }
-                   style={{ width: '100%' }}
-                   alt={ `${t("Author")}: ${author}` }
-                 />
-               </figure>
+               flexDirection: 'column',
+               alignItems: 'center',
+           }}>
+             <h3>
+               <a href={ programUrl } target='_blank' rel="noopener noreferrer">{ title }</a>
+             </h3>
+             <div style={{ marginBottom: '.5em' }}>
+               { schedule }
              </div>
-           </article>
-        )}
-     </MediaQuery>
-    );
-  }
+             <h4>{ author }</h4>
+           </header>
+           <div
+             style={{
+               alignSelf: 'center',
+           }}>
+             <figure style={{ position: 'relative' }} aria-label={ `${t("Author")}: ${author}` }>
+               <img
+                 src={ image }
+                 style={{ width: '100%' }}
+                 alt={ `${t("Author")}: ${author}` }
+               />
+             </figure>
+           </div>
+         </article>
+      )}
+   </MediaQuery>
+  )
 }
 
 PodcastCover.defaultProps = {
-  onClick: (e) => {},
+  onClick: () => {},
   minWidth: 1440,
 };
 
