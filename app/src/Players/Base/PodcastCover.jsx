494c502697c1dd40b58ac743883468637696653e
emibcn <emibcn@users.noreply.github.com>
2021-10-16 17:28:55 +0200

    Fix #125 - DeepSource: Fix unused vars (#126)
    * Format code with prettier and standardjs
    
    * Fix #125 - DeepSource: Add missing `propTypes`
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- app/src/Players/Base/PodcastCover.jsx
+++ app/src/Players/Base/PodcastCover.jsx
@@ -1,72 +1,67 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import { translate } from 'react-translate';
-import MediaQuery from 'react-responsive';
+import { translate } from 'react-translate'
+import MediaQuery from 'react-responsive'
 
-class PodcastCover extends PureComponent {
-  render() {
-    const {
-      minWidth,
-      image,
-      programUrl,
-      title,
-      author,
-      schedule,
-      t
-    } = this.props;
+function PodcastCover (props) {
+  const { minWidth, image, programUrl, title, author, schedule, t } = props
 
-    return (
-      <MediaQuery minWidth={ minWidth }>
-        { matches => (
-           <article
-             style={{
-               display: 'flex',
-               flexDirection: matches ? 'row' : 'column',
-               flexWrap: matches ? 'nowrap' : 'wrap',
-               justifyContent: 'center',
-               backgroundColor: 'white',
-               color: '#333',
-               padding: '1em',
-               borderRadius: '10px',
-             }}>
-             <header
-               style={{
-                 display: 'flex',
-                 flexDirection: 'column',
-                 alignItems: 'center',
-             }}>
-               <h3>
-                 <a href={ programUrl } target='_blank' rel="noopener noreferrer">{ title }</a>
-               </h3>
-               <div style={{ marginBottom: '.5em' }}>
-                 { schedule }
-               </div>
-               <h4>{ author }</h4>
-             </header>
-             <div
-               style={{
-                 alignSelf: 'center',
-             }}>
-               <figure style={{ position: 'relative' }} aria-label={ `${t("Author")}: ${author}` }>
-                 <img
-                   src={ image }
-                   style={{ width: '100%' }}
-                   alt={ `${t("Author")}: ${author}` }
-                 />
-               </figure>
-             </div>
-           </article>
-        )}
-     </MediaQuery>
-    );
-  }
+  return (
+    <MediaQuery minWidth={minWidth}>
+      {(matches) => (
+        <article
+          style={{
+            display: 'flex',
+            flexDirection: matches ? 'row' : 'column',
+            flexWrap: matches ? 'nowrap' : 'wrap',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            color: '#333',
+            padding: '1em',
+            borderRadius: '10px'
+          }}
+        >
+          <header
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}
+          >
+            <h3>
+              <a href={programUrl} target='_blank' rel='noopener noreferrer'>
+                {title}
+              </a>
+            </h3>
+            <div style={{ marginBottom: '.5em' }}>{schedule}</div>
+            <h4>{author}</h4>
+          </header>
+          <div
+            style={{
+              alignSelf: 'center'
+            }}
+          >
+            <figure
+              style={{ position: 'relative' }}
+              aria-label={`${t('Author')}: ${author}`}
+            >
+              <img
+                src={image}
+                style={{ width: '100%' }}
+                alt={`${t('Author')}: ${author}`}
+              />
+            </figure>
+          </div>
+        </article>
+      )}
+    </MediaQuery>
+  )
 }
 
 PodcastCover.defaultProps = {
-  onClick: (e) => {},
-  minWidth: 1440,
-};
+  minWidth: 1440
+}
 
 PodcastCover.propTypes = {
   image: PropTypes.string.isRequired,
@@ -74,8 +69,8 @@ PodcastCover.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   schedule: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-  minWidth: PropTypes.number.isRequired,
-};
+  t: PropTypes.func.isRequired,
+  minWidth: PropTypes.number
+}
 
-export default translate('PodcastCover')(PodcastCover);
+export default translate('PodcastCover')(PodcastCover)
