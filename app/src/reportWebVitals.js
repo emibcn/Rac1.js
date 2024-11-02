8ff54badf964818074d0ecbae3b5d514f04de8dd
emibcn <emi@algorismia.com>
2024-11-02 12:58:32 +0100

    Fix WebVitals new syntax

--- app/src/reportWebVitals.js
+++ app/src/reportWebVitals.js
@@ -1,12 +1,15 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals'
+import { MetricType } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry)
-    getFID(onPerfEntry)
-    getFCP(onPerfEntry)
-    getLCP(onPerfEntry)
-    getTTFB(onPerfEntry)
+    import("web-vitals")
+      .then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+        onCLS(onPerfEntry)
+        onINP(onPerfEntry)
+        onFCP(onPerfEntry)
+        onLCP(onPerfEntry)
+        onTTFB(onPerfEntry)
+      })
   }
 }
 
